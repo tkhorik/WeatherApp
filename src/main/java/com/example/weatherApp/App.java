e9c3a12e6d911eea42d70c52e67ea90ae98f8794
Tkhorik Dmitry <marvelmd@gmail.com>
2025-05-03 12:51:56 +0300

    feat: filter for patch implemented fix: also api token war removed from the repo

--- src/main/java/com/example/weatherApp/App.java
+++ src/main/java/com/example/weatherApp/App.java
@@ -1,22 +1,37 @@
 package com.example.weatherApp;
 
 import com.example.weatherApp.config.WebConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class App extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] { WebConfig.class };
+        return new Class<?>[]{WebConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] { WebConfig.class };
+        return new Class[]{WebConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/" };
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+        // Additional startup logic if needed
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        servletContext.addFilter("hiddenFieldFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
